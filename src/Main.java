2e3affa2fd111aed54c225a16fdaeae7dcad12cd
GeorgePopaA <poplike14@yahoo.com>
2023-01-05 21:25:39 +0200

    I Main.java file I have created an abstract class implemented in 2 inherited classes (car and Bus). Using abstract keyword, we cannot instantiate an object belonging of abstract class, Thus we substituted the abstract class with inheritanced classes and used their methods. That is Substitution Liskov Principle.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
 
+abstract class Vehicle{
+    abstract int getSpeed();
+    abstract int getCubicCapacity();
+}
+
+class Car extends Vehicle{
+    int getSpeed(){ return 100;}
+    int getCubicCapacity(){return 1900;}
+    boolean sHatchBack() {return true;}
+}
+
+class Bus extends Vehicle{
+    int getSpeed(){ return 100;}
+    int getCubicCapacity(){ return 1900;}
+    String getEmergencyExitLoc(){ return "Front";}
+}
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Vehicle vehicle = new Bus();
+        vehicle.getSpeed();
+        vehicle = new Car();
+        vehicle.getCubicCapacity();
     }
 }
